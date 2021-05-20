0c326abf6721648228fe57ad5f0e67fd4a701978
subcorporeal <subcorporeal@gmail.com>
2021-05-20 16:58:07 -0500

    Simple product-cart styles added

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -5,6 +5,7 @@ import 'components/input'
 import 'components/spinner'
 import 'components/sidebar'
 import temposCartImage from 'common/assets/shopping_cart.svg'
+import temposProductImage from 'common/assets/product_placeholder_2.svg'
 import { Component } from 'base/component'
 import { config } from 'common'
 
@@ -13,11 +14,13 @@ export class TemposCartComponent extends Component {
   init(context = {}) {
     this.global = context.global || window
     this.global.addEventListener(
-      'product-selected', this.onProductSelected.bind(this))
+      'product-selected',
+      this.onProductSelected.bind(this)
+    )
     this.storage = this.global.localStorage
 
-    const items = this.storage.getItem('items') ||  '{}'
-    this.items = JSON.parse(items) 
+    const items = this.storage.getItem('items') || '{}'
+    this.items = JSON.parse(items)
 
     return super.init(context)
   }
@@ -36,8 +39,9 @@ export class TemposCartComponent extends Component {
     <ark-sidebar side="right">
       <div slot="header">Carrito</div>
       <div data-content>
-        ${Object.values(this.items).map(
-          item => this.renderItem(item)).join('')}
+        ${Object.values(this.items)
+          .map((item) => this.renderItem(item))
+          .join('')}
       </div>
       <div class="${tag}__footer" slot="footer">
         <ark-button background="success"
@@ -50,15 +54,31 @@ export class TemposCartComponent extends Component {
     return super.render()
   }
 
+  // renderItem(item) {
+  //   return /* html*/ `
+  //   <ark-card title=${item.name} data-product-id="${item.id}">
+  //     <ark-input type="number" min="1" listen on-alter="onItemAltered"
+  //       value="${item.quantity}"></ark-input>
+  //     <div>Price: ${item.price}</div>
+  //     <ark-button background="danger" listen
+  //       on-click="onDeleteClicked">X</ark-button>
+  //   </ark-card>
+  //   `
+  // }
+
   renderItem(item) {
-    return `
-    <ark-card title=${item.name} data-product-id="${item.id}">
-      <ark-input type="number" min="1" listen on-alter="onItemAltered"
-        value="${item.quantity}"></ark-input>
-      <div>Price: ${item.price}</div>
-      <ark-button background="danger" listen
-        on-click="onDeleteClicked">X</ark-button>
-    </ark-card>
+    return /* html*/ `
+      <div class="product-card" data-product-id="${item.id}">
+        <div class="product-card__image" style="background-image: url(${temposProductImage})"></div>
+        <div class="product-card__info">
+        <h1 class="product-card__title">${item.name}</h1>
+          <ark-input inline label="Quantity:" type="number" min="1" listen on-alter="onItemAltered"
+          value="${item.quantity}"></ark-input>
+          <div class="product-card__price"> Price: <span>${item.price}$<span></div>
+        </div>
+         <ark-button background="danger" listen
+       on-click="onDeleteClicked">×</ark-button>
+      </div>
     `
   }
 
@@ -72,7 +92,7 @@ export class TemposCartComponent extends Component {
 
   onProductSelected(event) {
     event.stopPropagation()
-    const item = Object.assign({quantity: '0'},  event.detail)
+    const item = Object.assign({ quantity: '0' }, event.detail)
     if (!this.items[item.id]) {
       this.items[item.id] = item
     }
@@ -90,7 +110,7 @@ export class TemposCartComponent extends Component {
     event.stopPropagation()
     const order = {
       id: 'ABC123',
-      items: Object.assign({}, this.items)
+      items: Object.assign({}, this.items),
     }
     this.emit('order-created', order)
     this.items = {}
@@ -136,6 +156,76 @@ const styles = /* css */ `
 .${tag}__footer {
   display: grid;
   align-items: center;
+}
+.ark-sidebar__menu{
+  min-width:300px;
+  width: 30%;
+}
+.ark-sidebar__body{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  width:100%;
+  padding: 0;
+}
+.ark-sidebar__body [data-content]{
+  width:100%;
+}
+.product-card{
+  display:flex;
+  align-items: center;
+  justify-content:space-between;
+  gap:2rem;
+  padding:0.5rem;
+  border-top: 2px solid #d8d8d8;
+  border-bottom: 2px solid #d8d8d8;
+}
+.product-card__image{
+  width:70px;
+  height:70px;
+  background-repeat:no-repeat;
+  background-size: contain;
+  background-position:center;
+}
+
+.product-card__title{
+  font-size: 1.3rem;
+}
+
+.product-card__info .ark-input{
+  margin:0;
+  padding:0;
+}
+.product-card__info .ark-input input{
+  padding:0rem;
+  box-shadow:none;
+  width: 3rem;
+  text-align: center;
+}
+.product-card__info .ark-input__text{
+  font-weight:700;
+  font-size: 0.9rem;
+}
+.product-card__price{
+  font-size: 0.9rem;
+  font-weight: 700;
+  margin-top:0.5rem;
+}
+.product-card__price span{
+  font-weight: 400;
+  margin-left:2.5rem;
+}
+.product-card .ark-button{
+  align-self: baseline;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  font-size: 1.5rem;
+}
+.product-card .ark-button__body{
+  position:relative;
+  left:1px;
+  bottom:1px;
 }
 `
 
